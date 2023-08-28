ff7878a56524f263bfa58c963fc87fce9d814a11
Weston Walker <walk8919@gmail.com>
2023-08-28 16:34:25 -0500

    Add project files.

---
+++ Spark.UI/Shared/Components/Variants.cs
@@ -0,0 +1,20 @@
+﻿namespace Spark.UI.Shared.Components;
+
+public enum AlertVariant
+{
+	Default,
+	Success,
+	Info,
+	Warning,
+	Destructive,
+}
+
+public enum ButtonVariant
+{
+    Default,
+    Destructive,
+    Outline,
+    Secondary,
+    Ghost,
+    Link
+}
