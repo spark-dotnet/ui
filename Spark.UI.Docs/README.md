ff1b9373a515ccbf51926c048b1922ed25fd7fdf
Weston Walker <walk8919@gmail.com>
2023-08-29 20:25:29 -0500

    more changes

---
+++ Spark.UI.Docs/README.md
@@ -0,0 +1,7 @@
+﻿# spark.ui
+Beautiful Blazor Components
+
+## Getting Started
+1. npm install
+2. npx tailwindcss -i ./Resources/Css/globals.css -o ./wwwroot/css/styles.css --watch
+3. dotnet watch
---
+++ Spark.UI/Components/Variants.cs
@@ -0,0 +1,28 @@
+﻿namespace Spark.UI.Components;
+
+public enum AlertVariant
+{
+	Default,
+	Success,
+	Info,
+	Warning,
+	Destructive,
+}
+
+public enum BadgeVariant
+{
+	Default,
+	Secondary,
+	Outline,
+	Destructive,
+}
+
+public enum ButtonVariant
+{
+    Default,
+    Destructive,
+    Outline,
+    Secondary,
+    Ghost,
+    Link
+}
